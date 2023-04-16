0100f58501469fd8b99f4eca7e515098e4f6c55c
Victoria Keibel <104903461+VictoriaKeibel@users.noreply.github.com>
2023-04-16 15:02:26 -0300

    INSERINDO VALORES
    VALORES INSERIDOS EM SUAS DETERMINADAS TABELAS DE FORMA MANUAL.

---
+++ INSERINDO VALORES.sql
@@ -0,0 +1,216 @@
+
+/* INSERINDO VALORES PARA PRODUTO */
+
+INSERT INTO PRODUTO VALUES(NULL,'Lego_Marvel_vingadores','Jogos_ps4_novos',0,37.50,80,42.50);
+INSERT INTO PRODUTO VALUES(NULL,'Fallout_4','Jogos_ps4_novos',0,65,109.99,44.99);
+INSERT INTO PRODUTO VALUES(NULL,'Sherloc_ Holmes_Crime_&_Punishment','Jogos_ps4_novos',0,65,109.99,44.99);	
+INSERT INTO PRODUTO VALUES(NULL,'GTA_V','Xbox_One_Usado',0,99.99,99.99,0);
+INSERT INTO PRODUTO VALUES(NULL,'Rayman_legends','Xbox_One_360_NOVO',0,69.90,109.99,40);
+INSERT INTO PRODUTO VALUES(NULL,'Turbo_super_stunt_squas', 'Xbox_One_360_USADO',0,25,60,35);
+INSERT INTO PRODUTO VALUES(NULL,'Minecraft_dungeons_hero_edition','Xbox_One_360_NOVO',0,48.90,109.90,61);
+INSERT INTO PRODUTO VALUES(NULL,'Lego_Batman_2','Xbox_One_Usado',1,37.50,80,42.50);
+INSERT INTO PRODUTO VALUES(NULL,'dragonball_xenomverse_2','Jogos_ps4_USADO',0,15,75,60);
+INSERT INTO PRODUTO VALUES(NULL,'Combo_Spider_Man_+_ratchet_&_Clank','Jogos_ps4_USADO',1,80,160,80);
+INSERT INTO PRODUTO VALUES(NULL,'Lego Marvel super Heroes','Xbox_One_360_USADO',1,37.50,90,52.50);
+INSERT INTO PRODUTO VALUES(NULL,'God_of_war_ascension','Jogos_ps3_usados',1,10,120,110);
+INSERT INTO PRODUTO VALUES(NULL,'God_of_war 3','Jogos_ps3_usados',1,10,120,110);
+INSERT INTO PRODUTO VALUES(NULL,'God_of_War','Jogos_ps3_usados',1,10,120,110);
+INSERT INTO PRODUTO VALUES(NULL,'Borderlands_The_handsome_collection','Jogos_ps4_novos',1,10,100,90);
+INSERT INTO PRODUTO VALUES(NULL,'Pack_Heavy_rain_&_beyond_two_souls_collection','Jogos_ps4_novos',1,65,80,15);
+INSERT INTO PRODUTO VALUES(NULL,'The_dwarves','Jogos_ps4_novos',1,65,89.99,24.99);
+INSERT INTO PRODUTO VALUES(NULL,'Pillars of eternity complete edition','Jogos_ps4_USADO',1,65,65,0);
+INSERT INTO PRODUTO VALUES(NULL,'The dark pictures anthology: Man of Medan','Jogos PS4 usados',1,65,65,0);
+INSERT INTO PRODUTO VALUES(NULL,'Song of the deep','Jogos ps4 novos',1,65,80,15);
+INSERT INTO PRODUTO VALUES(NULL,'GTA V',' Xbox 360 novos',0,86.45,150,63.55);
+INSERT INTO PRODUTO VALUES(NULL,'Mortal Kombat - Komplete edition','Jogos XBOX 360 usado',0,16.66,80,63.34);
+INSERT INTO PRODUTO VALUES(NULL,'Fifa copa do mundo 2010','Jogos X 360 usado',0,16.66,50,33.34);
+INSERT INTO PRODUTO VALUES(NULL,'Minecraft','Jogos X 360 usado',0,36.25,120,83.75);
+INSERT INTO PRODUTO VALUES(NULL,'Battlefield 4','Xbox 360 usado',0,36.25,60,23.75);
+INSERT INTO PRODUTO VALUES(NULL,'Call of duty ghosts','Jogos X 360 usado',0,40,100,60);
+INSERT INTO PRODUTO VALUES(NULL,'Final fantasy Xiii','Jogos X 360 usado',1,16.66,70,53.34);
+INSERT INTO PRODUTO VALUES(NULL,'Destiny','usado','xbox 360',0,36.25,55,18.75);
+INSERT INTO PRODUTO VALUES(NULL, 'Fifa 14','usado','xbox 360',0,16.66,55,38.34);
+INSERT INTO PRODUTO VALUES(NULL, 'Just dance 2018 ','usado','xbox 360',0,16.66,70,53.34);
+INSERT INTO PRODUTO VALUES(NULL,'Kinect adventures','usado','xbox 360',1,16.66,18.99,2.33);
+INSERT INTO PRODUTO VALUES(NULL, 'Fifa 2014','usado','xbox one',1,49.90,49.90,0);
+INSERT INTO PRODUTO VALUES(NULL, 'Battlefield V','usado','xbox one',1,99.90,99.90,0);
+INSERT INTO PRODUTO VALUES(NULL,'Monster Hunter world', 'usado','xbox one',1,99.90,99.90,0);
+INSERT INTO PRODUTO VALUES(NULL,'God of War hits ','NOVO','PS4',0,63.90,109.90,46);
+INSERT INTO PRODUTO VALUES(NULL,'Doom Eterna','NOVO','PS4',0,59.99,100,40);
+INSERT INTO PRODUTO VALUES(NULL,'Shadow of the Colossus','NOVO','PS4',0,77.90,129,51.10);
+INSERT INTO PRODUTO VALUES(NULL,'Horizon zero Dawn ','NOVO','PS4',0,59.99,109.90,49.91);
+INSERT INTO PRODUTO VALUES(NULL,'Ratchet & Clank hits','NOVO','PS4',1,50.79,100,49.21);
+INSERT INTO PRODUTO VALUES(NULL,'The last of us remasterizado','NOVO','PS4',0,79.90,109.90,30);
+INSERT INTO PRODUTO VALUES(NULL,'Uncharted 4 a thiefs ends','NOVO','PS4',0,62.99,109.90,46.91);
+INSERT INTO PRODUTO VALUES(NULL,'Lego Star Wars','novos','ps3',1,189.90,189.90,0);
+INSERT INTO PRODUTO VALUES(NULL,'Gears 5 + chaveiro','novo','xbox one',1,69.89,119.90,51.01);
+INSERT INTO PRODUTO VALUES(NULL,'PES 2020', 'novo','xbox one',1,68.99,109.90,40.91);
+INSERT INTO PRODUTO VALUES(NULL,'Devil May cry 5 ', 'novo','xbox one',1,116.74,199.90,83.16);
+INSERT INTO PRODUTO VALUES(NULL,' Lego Star Wars the complete saga','usado','xbox 360',0,30,80,50);
+INSERT INTO PRODUTO VALUES(NULL,'Kinect dance Central ','usado','xbox 360',0,23,60,37);
+INSERT INTO PRODUTO VALUES(NULL,'Kinect sports segunda temporada','usado','xbox 360'0,15,45,30);
+INSERT INTO PRODUTO VALUES(NULL,'Pes 2014 ','usado','xbox 360',0,20,65,45);
+INSERT INTO PRODUTO VALUES(NULL,' Lego Dc super villains','novo','xbox one',1,78.90,109.90,31);
+INSERT INTO PRODUTO VALUES(NULL,'Minecraft Playstation 3 edition','novo','ps3',1,169.90,249.90,80);
+INSERT INTO PRODUTO VALUES(NULL,'Darksiders','usado','ps3',1,10,60,50);
+INSERT INTO PRODUTO VALUES(NULL,'Skyrim','usado','ps3',1,55,60,15);
+INSERT INTO PRODUTO VALUES(NULL,'Assassins Creed 2', 'usado','ps3',1,40,40,0);
+INSERT INTO PRODUTO VALUES(NULL, 'assassins Creed rogue','usado','ps3',1,50,50,0);
+INSERT INTO PRODUTO VALUES(NULL,'Call of duty World at War', 'usado','xbox 360',0,69.90,69.90,0);
+INSERT INTO PRODUTO VALUES(NULL,'Far Cry 3','novo', 'xbox 360',1,67.99,109.90,41.91);
+INSERT INTO PRODUTO VALUES(NULL,' Racedriver grid','usado','ps3',1,18.75,50,31.25);
+INSERT INTO PRODUTO VALUES(NULL,' Racedriver Grid 2','usado','ps3',1,18.75,65,41.25);
+INSERT INTO PRODUTO VALUES(NULL,'Formula 1 2011','usado','ps3',1,18.75,60,36.25);
+INSERT INTO PRODUTO VALUES(NULL, 'Dirt 3','usado','ps3',1, 18.75,60,41.25);
+INSERT INTO PRODUTO VALUES(NULL,'Pes 2014', 'usado','ps3',1,18.75,60,41.25);
+INSERT INTO PRODUTO VALUES(NULL,'Gran turismo 6','usado','ps3',1,18.75,70,51.25);
+INSERT INTO PRODUTO VALUES(NULL, 'the Sims 3','usado','ps3',0,18.75,75,56.25);
+INSERT INTO PRODUTO VALUES(NULL,' FIFA 12','usado','ps3',1,18.75,45, 26.25);
+INSERT INTO PRODUTO VALUES(NULL,'Red dead redemption','usado','xbox 360',0,50,90,40);
+INSERT INTO PRODUTO VALUES(NULL, 'Fifa 2013', 'usado','xbox 360',0,40,60,20);
+INSERT INTO PRODUTO VALUES(NULL, 'Call of duty Black ops','usado','xbox 360',0,35,60,25);
+INSERT INTO PRODUTO VALUES(NULL,'Mortal Kombat vs Dc universe', 'usado','xbox 360',2,35,95,60);
+INSERT INTO PRODUTO VALUES(NULL, 'forza Horizon', 'usado','xbox 360',0,35,90,60);
+INSERT INTO PRODUTO VALUES(NULL,'MidnightClub Los Angeles','usado','xbox 360',0,35,90,60);
+INSERT INTO PRODUTO VALUES(NULL,'Call of duty Black ops 2','usado','xbox 360',0,35,90,60);
+INSERT INTO PRODUTO VALUES(NULL,'Devil May cry - DmC','usado','xbox 360',0,40,70,30);
+INSERT INTO PRODUTO VALUES(NULL,'Batman Arkham asylum','usado','xbox 360',2,35,80,45);
+INSERT INTO PRODUTO VALUES(NULL,'Pes 2013', 'usado','xbox 360',0,25,80,55);
+INSERT INTO PRODUTO VALUES(NULL,'Pes 2012','usado','xbox 360',1,25,70,45);
+INSERT INTO PRODUTO VALUES(NULL,'Resident Evil 2 Revelations ' ,'usado','xbox 360',0,35,70,25);
+INSERT INTO PRODUTO VALUES(NULL,'Dragon age inquisition','usado','xbox 360',0,35,60,25);
+INSERT INTO PRODUTO VALUES(NULL,'Marvel Spider-Man ','NOVO','PS4',0,129.90,159,29.10);
+INSERT INTO PRODUTO VALUES(NULL,'Marvel Spider-Man Miles Morales','NOVO','PS4',0,118.90,159,40.10);
+INSERT INTO PRODUTO VALUES(NULL,'Guitar hero World tour ','usado','ps3',0,10,30,20);
+INSERT INTO PRODUTO VALUES(NULL,'Assassin s creed','usado','xbox 360',1,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'Fable journey','usado','ps3',0,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'Alien Isolation Nostromo Edition','usado','ps3',0,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL, 'Assassins Creed Revelations','usado','ps3',1,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'Resistance 3','usado','ps3',1,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'God of war Collection','usado','ps3',1,10,120,110);
+INSERT INTO PRODUTO VALUES(NULL,'ALL stars Battle royale','usado','ps3',0,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'Duke nukem forever','usado','ps3',1,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'Splatterhouse','usado','ps3',1,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'Dead Space 2 ','usado','ps3',1,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'Resident Evil anthology ','usado','ps3',1,35,90,55);
+INSERT INTO PRODUTO VALUES(NULL,'Resident Evil operation racoon city','usado','ps3',1,35,95,60);
+INSERT INTO PRODUTO VALUES(NULL,'Resistance 2','usado','ps3',1,35,70,35);
+INSERT INTO PRODUTO VALUES(NULL,'Metal Gear Solid HD Collection','usado','ps3',1,35,135,100);
+INSERT INTO PRODUTO VALUES(NULL,'Halo reach','usado','xbox 360',1,50,110,60);
+INSERT INTO PRODUTO VALUES(NULL,'Injustice (ultimate Edition)','usado','xbox 360',1,50,110,60);
+INSERT INTO PRODUTO VALUES(NULL,'Lego Jurassic world','usado','xbox 360',1,50,150,100);
+INSERT INTO PRODUTO VALUES(NULL,'Metalgear Rising ','usado','xbox 360',1,50,80,30);
+INSERT INTO PRODUTO VALUES(NULL,'Syndicate','usado','xbox 360',0,50,90,40);
+INSERT INTO PRODUTO VALUES(NULL,'Batman Arkham city','usado','xbox 360',1,50,90,40);
+INSERT INTO PRODUTO VALUES(NULL,'Fifa 19','usado','xbox 360',0,70,120,50)
+INSERT INTO PRODUTO VALUES(NULL,'Just dance 2016 ','usado','xbox 360',1,60,60,0);
+INSERT INTO PRODUTO VALUES(NULL,'GTA V','usado','xbox 360',1,70,130,60);
+INSERT INTO PRODUTO VALUES(NULL,'Spider-man','usado','PS4',1,41,120,79);
+INSERT INTO PRODUTO VALUES(NULL,'Horizon Zero Dawn','usado','PS4',1,41,80,39);
+INSERT INTO PRODUTO VALUES(NULL,'Assassin s Creed unity','NOVO','PS4',1,65,90,25);
+INSERT INTO PRODUTO VALUES(NULL,'Ghost recon trilogy','usado','xbox 360',1,5,90,40);
+INSERT INTO PRODUTO VALUES(NULL,'Uncharted 4 - a thief s end','usado','PS4',1,40,80,40);
+INSERT INTO PRODUTO VALUES(NULL,'Nioh','usado','PS4',1,41,80,39);
+INSERT INTO PRODUTO VALUES(NULL,'Crysis 3','usado','xbox 360',1,50,70,20);
+INSERT INTO PRODUTO VALUES(NULL,'Mafia 3 ','usado','PS4',1,41,80,39);
+INSERT INTO PRODUTO VALUES(NULL,'Fallout 4','usado','PS4',1,50,70,20);
+INSERT INTO PRODUTO VALUES(NULL,'God of war','usado','PS4',0,41,80,39);
+INSERT INTO PRODUTO VALUES(NULL,'Gears of war 2 ','usado','xbox 360',0,50,80,30);
+INSERT INTO PRODUTO VALUES(NULL,'Tom clancy s Ghost Recon','usado','PS4',1,45,90,45);
+INSERT INTO PRODUTO VALUES(NULL,'Ratchet & Clank','usado','PS4',1,41,70,29);
+INSERT INTO PRODUTO VALUES(NULL,'The last of us Remasterizado','usado','PS4',1,41,75,34);
+INSERT INTO PRODUTO VALUES(NULL,'Cyberpunk 2077','usado','PS4',0,41,80,39);
+INSERT INTO PRODUTO VALUES(NULL,'Destiny the taken king','usado','xbox 360',1,50,80,30);
+INSERT INTO PRODUTO VALUES(NULL,'Devil May Cry','usado','ps3',1,10,80,70);
+INSERT INTO PRODUTO VALUES(NULL,'Gran turismo 5 prologue ','usado','ps3',1,10,50,40);
+INSERT INTO PRODUTO VALUES(NULL,'Gran turismo 5  XL Edition','usado','ps3',1,18.75,80,61.25);
+INSERT INTO PRODUTO VALUES(NULL,'GTA IV ','usado','ps3',1,10,70,60);
+INSERT INTO PRODUTO VALUES(NULL,'Fallout 3 ','usado','ps3',1,10,75,60);
+INSERT INTO PRODUTO VALUES(NULL,'Fifa soccer 10','usado','ps3',1,10,30,20);
+INSERT INTO PRODUTO VALUES(NULL,'Assassin s Creed 2','usado','ps3',2,10,60,50);
+INSERT INTO PRODUTO VALUES(NULL,'Street fight IV ','usado','ps3',1,10,55,45);
+INSERT INTO PRODUTO VALUES(NULL,'Batman Arkham origins','usado','ps3',2,10,60,50);
+INSERT INTO PRODUTO VALUES(NULL,'Pes 2017','usado','ps3',1,10,60,50);
+INSERT INTO PRODUTO VALUES(NULL,'Farcry 3 ','usado','ps3',1,10,80,60);
+INSERT INTO PRODUTO VALUES(NULL,'Dark souls','usado','ps3',1,10,90,80);
+INSERT INTO PRODUTO VALUES(NULL,'Need for Speed Rivals','usado','ps3',1,18.75,60,41.25);
+INSERT INTO PRODUTO VALUES(NULL,'Devil May cry hd colletion ','usado','ps3',1,10,80,70);
+INSERT INTO PRODUTO VALUES(NULL,'FIFA 14','usado','ps3',0,10,50,40);
+INSERT INTO PRODUTO VALUES(NULL,'Fallout New Vegas','usado','ps3',1,10,70,60);
+INSERT INTO PRODUTO VALUES(NULL,'Demons souls','usado','ps3',1,10,90,80);
+INSERT INTO PRODUTO VALUES(NULL,'Dragons dogma','usado','ps3',1,10,80,70);
+INSERT INTO PRODUTO VALUES(NULL,'The evil within','usado','ps3',1,10,60,50);
+INSERT INTO PRODUTO VALUES(NULL,'Dead Island','usado','ps3',1,10,60,50);
+INSERT INTO PRODUTO VALUES(NULL,'Dead Space ','usado','ps3',1,10,60,50);
+INSERT INTO PRODUTO VALUES(NULL,'F. E. A. R 3','usado','ps3',1,10,65,55);
+INSERT INTO PRODUTO VALUES(NULL,'Resident Evil revelations','usado','ps3',1,10,70,60)
+INSERT INTO PRODUTO VALUES(NULL,'Assassin s Creed 4 Black fla','usado','ps3',1,10,80,70);
+INSERT INTO PRODUTO VALUES(NULL,'Crysis 3','usado','ps3',1,10,65,55);
+INSERT INTO PRODUTO VALUES(NULL,'The club ','usado','ps3',1,10,50,40);
+INSERT INTO PRODUTO VALUES(NULL,'Dead Island','usado','ps3',1,10,50,40);
+INSERT INTO PRODUTO VALUES(NULL,'PES 2008','usado','ps3',1,10,60,50);
+INSERT INTO PRODUTO VALUES(NULL,'Ratchet & Clank ALL 4 one','usado','ps3',1,10,75,65);
+INSERT INTO PRODUTO VALUES(NULL,'The little BIG planet 2','usado','ps3',1,10,70,60);
+INSERT INTO PRODUTO VALUES(NULL,'Far Cry 2 ','usado','ps3',1,10,50,40);
+INSERT INTO PRODUTO VALUES(NULL,'PES 2018','usado','xbox 360',0,55,200,145);
+INSERT INTO PRODUTO VALUES(NULL,'Call of duty modern warfare 3','usado','xbox 360',1,55,130,75)
+INSERT INTO PRODUTO VALUES(NULL,'Call of duty modern warfare 4','usado','xbox 360',1,55,120,65);
+INSERT INTO PRODUTO VALUES(NULL,'Horizon Forbidden West ','usado','PS4',1,80,180,100);
+INSERT INTO PRODUTO VALUES(NULL,'Pes 2021' 'usado', 'xbox one',0,52.39,100,47.61);
+INSERT INTO PRODUTO VALUES(NULL,'Bloodborne ','usado','PS4',2,50,80,30);
+INSERT INTO PRODUTO VALUES(NULL,'Watch Dogs ','usado','PS4',2,50,95,45);
+INSERT INTO PRODUTO VALUES(NULL,'Crash bandicoot N-sane trilogy ','usado','PS4',1,35,99,64);
+INSERT INTO PRODUTO VALUES(NULL,'Uncharted Nathan Drake collection','usado','PS4',1,50,85,35);
+INSERT INTO PRODUTO VALUES(NULL,'God of War ragnarok',4,246.31,270,23.69);
+INSERT INTO PRODUTO VALUES(NULL,'Sonic generations','usado','xbox 360',0,45,120,75);
+
+/* INSERINDO VALORES PARA CLIENTE */
+
+
+INSERT INTO CLIENTE VALUES (NULL, 'LUCAS MED', NULL),
+     (NULL,'GISELLE',NULL),
+     (NULL,'Gustavo Coelho Wantel',NULL),
+     (NULL,'FABIO OLX', NULL),
+     (NULL,'EMANUELLA', NULL),
+     (NULL,'LUCAS',NULL),
+     (NULL,'RICKELMY',NULL),
+     (NULL,'HEBERT',NULL),
+     (NULL,'JUNIOR',NULL);
+	 
+	 
+	 /* INSERINDO VALORES PATA TELEFONE */
+	 
+	 INSERT INTO TELEFONE VALUES (NULL,'CEL',8499334242,1 ),
+     (NULL,'CEL',87996128627,2),
+     (NULL,'CEL',87996478939,3),
+     (NULL,'CEL',8781783396,4),
+     (NULL,'CEL',87988715584,5),
+     (NULL,'CEL',87988399419,6),
+     (NULL,'CEL',74988248548,7),
+     (NULL,'CEL',8499334242,8),
+     (NULL,'CEL',8182597371,9);
+	 
+	/*  INSERINDO VALORES PRA ENDEREÇO */
+	 
+	 INSERT INTO ENDERECO VALUES (NULL, 'Rua Floriano Peixoto','Bairro Rio Corrente','Petrolina','Pe',123,1),
+     (NULL,'Avenida da Integração','Vila Mocó','Petrolina','Pe',456,2),
+     (NULL,'Travessa das Mangueiras','Jardim Amazonas','Petrolina','Pe',789,3),
+     (NULL,'Rua do Sol','Jardim Maravilha','Petrolina','Pe',101,4),
+     (NULL,'Avenida da Liberdade','Dom Avelar','Petrolina','Pe',222,5),
+     (NULL,'Rua das Acácias','Areia Branca','Petrolina','Pe',333,6),
+     (NULL,'Travessa dos Lírios','Pedra Linda','Petrolina','Pe',444,7),
+     (NULL,'Avenida dos Ipês','Jardim Imperial','Petrolina','Pe',555,8),
+     (NULL,'Rua dos Girassóis','Jardim das Águas','Petrolina','Pe',666,9);
+	 
+	 
+	/* INSERINDO VALORES PARA PEDIDO */
+	 
+	 INSERT INTO PEDIDO VALUES (NULL,'GTA_V',125,0,20220423,'DINHEIRO',6);
+     INSERT INTO PEDIDO VALUES (NULL,'PES 2021',100,12,20220531,'CARTÃO',1),
+     (NULL,'PRÉ-VENDA GOD OF WAR ragnarok',270,5,20220809,'DINHEIRO',2),
+     (NULL,'Skate 3, PES 2018 ',115,0,20221014,'Dinheiro',3),
+     (NULL,'Sonic generations',115,0,20221025,'DINHEIRO',4),
+     (NULL,'Cyberpunk 2077',115,10,20221108,'PIX',5),
+     (NULL,'God of war ',115,0,20221108,'DINHEIRO',7),
+     (NULL,'FIFA 14',115,7,20221123,'DINHEIRO',8),
+     (NULL,'GTA V ',115,0,20230109,'DINHEIRO',9);
